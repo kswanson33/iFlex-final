4a93e53f18657935ddce437f3ae61aed9f7dfe6e
Kendra Swanson <kendraswan@gmail.com>
2018-11-01 13:04:07 -0500

    initialize exercise and workout structs

---
+++ iFlex/Exercise.swift
@@ -0,0 +1,9 @@
+//
+//  Exercise.swift
+//  iFlex
+//
+//  Created by Kendra Swanson on 11/1/18.
+//  Copyright © 2018 Kendra Swanson, Christina Stellwagen, Carla Beghin, Benjamin Gu. All rights reserved.
+//
+
+import Foundation
