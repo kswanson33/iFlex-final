e1f19665f0cf95651b7d8f24edc8a5c282617574
labuser <labuser@eusadminss-iMac-46.local>
2018-11-19 23:09:41 -0600

    updated with Benji's codenote that we need to connect to the storyboard... I didn't do this because it isn't the most recent version-- but the outlets need to be connected and the collection view needs to be set up

---
+++ iFlex/CellContent.swift
@@ -0,0 +1,9 @@
+//
+//  CellContent.swift
+//  iFlex
+//
+//  Created by labuser on 11/19/18.
+//  Copyright © 2018 Kendra Swanson, Christina Stellwagen, Carla Beghin, Benjamin Gu. All rights reserved.
+//
+
+import Foundation
--- iFlex/browseNowScreen.swift
+++ iFlex/browseNowScreen.swift
@@ -8,28 +8,47 @@
 
 import UIKit
 
-class browseNowScreen: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+class browseNowScreen: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate  {
+        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            return Workouts.count
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+            let newCell = collectionView.dequeueReusableCell(withReuseIdentifier: "exerCell", for: indexPath) as! exCell
+            
+            
+            newCell.title.text = Workouts[indexPath.row].title
+            
+            
+            
+            return newCell
+        }
+        
+        
+        var Workouts: [Workout] = []
+        
+        
+        
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            var Wrkout1 = Workout(title: "A", time: TimeInterval(), exercises: [], difficulty: 1, tags: [], priv: false, numOfExercises: 5, sharedWith: [], description: "hello", icon: WorkoutTarget.core)
+            var Wrkout2 = Workout(title: "B", time: TimeInterval(), exercises: [], difficulty: 1, tags: [], priv: false, numOfExercises: 5, sharedWith: [], description: "hello", icon: WorkoutTarget.core)
+            Workouts = [Wrkout1, Wrkout2]
+            workoutCollection.delegate = self
+            workoutCollection.dataSource = self
+            //        WorkoutTable.register(UITableViewCell.self, forCellReuseIdentifier: "exerCell")
+            // Do any additional setup after loading the view, typically from a nib.
+        }
+        
+        override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+            // Dispose of any resources that can be recreated.
+        }
+        
+        @IBOutlet weak var workoutCollection: UICollectionView!
+        
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
+    //add second screen that takes in data, and fetch local data to populate cells use loadlocal to get local data, add to favorites
